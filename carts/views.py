9168b8510e1f08cfb75d395bbc78404068125d15
abdullah <apple@apples-MacBook-Air.local>
2024-07-04 10:54:16 +0500

    Fix worning and issues

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
-
+from django.core.exceptions import ObjectDoesNotExist
 
 # private function
 def _cart_id(request):
@@ -63,6 +63,8 @@ def remove_cart_item(request, product_id):
 
 def carts(request, total=0, quantity=0, cart_item=None):
     try:
+        tex = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:

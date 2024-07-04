b05c8a36b704441c6be7f6bf36858d2ca4cfec09
abdullah <apple@apples-MacBook-Air.local>
2024-07-04 11:33:15 +0500

    Search funcatiality complete

--- store/views.py
+++ store/views.py
@@ -3,6 +3,8 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.http import HttpResponse
+from django.db.models import Q
 
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -44,3 +46,17 @@ def product_details(request, category_slug, product_slug):
         'in_cart' : in_cart,
     }
     return render(request, 'store/product_details.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(product_name__icontains=keyword) | Q(description__icontains=keyword))
+            products_count = products.count()
+
+    context = {
+        'products' : products,
+        'products_count' : products_count,
+    }
+    return render(request, 'store/store.html', context)

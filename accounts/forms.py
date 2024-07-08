fb5afda53e69ae67937beca89ff0b515a28d718c
abdullah <apple@apples-MacBook-Air.local>
2024-07-08 10:22:52 +0500

    set register form to forms, init, and model assine css

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,33 @@ from django import forms
 from .models import Account
 
 class RegisterationForm(forms.ModelForm):
+   
+   # plaaceholder and class name, 1nd way to assine
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Enter Password',
+        'class' : 'form-control'
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Confirm Password',
+        'class' : 'form-control'
+    }))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    # plaaceholder and class name all fildes by loop, 2nd way to assine
+    def __init__(self, *args, **kwargs):
+        super(RegisterationForm, self).__init__(*args, **kwargs)
+
+        #placeholder for all fields
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        
+
+
+        #  add class name all fileds for css
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'

6e53ae45fed86a32801ebd9c8613414136b07bb2
abdullah <apple@apples-MacBook-Air.local>
2024-07-11 13:33:49 +0500

    Set the multiple images

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+
+from orders.models import OrderProduct
+from .models import Product, ProductGallery
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
@@ -35,18 +37,30 @@ def store(request, category_slug=None):
 
 def product_details(request, category_slug, product_slug):
     try:
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug) # category is the forenkey of category table and slug is field inside category table
-        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists() # cart is foreing key of cart model and cart id is exixt in cart model
-
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
-        
+
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
-        'single_product' : single_product,
-        'in_cart' : in_cart,
+        'single_product': single_product,
+        'in_cart'       : in_cart,
+        'orderproduct': orderproduct,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_details.html', context)
-
 
 def search(request):
     if 'keyword' in request.GET:
